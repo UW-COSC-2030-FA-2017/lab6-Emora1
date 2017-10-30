259ae5d7d38d405b97e6db29d72049353ec3196b
Evan Mora <emora1@uwyo.edu>
2017-10-30 11:30:57 -0600

    done
    done

--- SortedDriver.cpp
+++ SortedDriver.cpp
@@ -1,6 +1,6 @@
 // SortedDriver.cpp
 
-// tom bailey   1445  25 mar 2014
+// Evan Mora
 // Construct sorted sequences and call functions that 
 //   process the sorted sequences.
 
@@ -63,9 +63,49 @@ getWords(size_t numWords, size_t wordLength, string alphabet)
 double
 mostIsolated(vector<double> & number)
 {
-	// STUB  STUB  STUB
-	return -123.456;
+	int mostIsolatedLoc(0);
+	double magnitude(0);
+	for (int i(0); i <number.size(); i ++)
+		if (i == 0)
+		{
+			double temp1 = number[i] - number[i + 1];
+			checkNegative(temp1);
+			if (temp1 > magnitude)
+			{
+				magnitude = temp1;
+				mostIsolatedLoc = i;
+			}
+		}
+		else if (i == number.size() - 1)
+		{
+			double temp2 = numnber[i] - number[i - 1];
+			checkNegative(temp2);
+			if (temp2 > magnitude)
+			{
+				magnitude = temp2;
+				mostIsolatedLoc = i;
+			}
+		}
+		else {
+
+			double temp1 = number[i] - number[i - 1];
+			double temp2 = number[i] - number[i + 1];
+			checkNegitive(temp2);
+			checkNegitive(temp1);
+			if (temp1 < temp2) {
+				if (temp1 > magnitude) {
+					magnitude = temp1;
+					mostIsolatedLocation = i;
+				}
+			}
+			else if (temp2 > magnitude) {
+				magnitude = temp2;
+				mostIsolatedLocation = i;
+			}
+		}
 }
+return number[mostIsolatedLoc];
+	}
 
 
 // pre:  A and B are sorted.
@@ -74,8 +114,32 @@ mostIsolated(vector<double> & number)
 int
 unmatched(list<string> & A, list<string> & B)
 {
-	// STUB  STUB  STUB
-	return -1;
+	//setup
+	int test(0);
+	list<string>::iterator AList = A.begin();
+	list<string>::iterator BList = B.begin();
+	bool has = false;
+
+	
+	for (AList; AList != A.end(); AList++) {
+		has = false;
+		while (BList != B.end()) {
+		if (*AList == *BList) {
+		has = true;
+		break;
+			}
+			else if (*AList < *BList) {
+				B.pop_front();
+				break;
+			}
+			else if (*BList<*AList)
+				BList++;
+		}
+		if (has == false) {
+			test++;
+		}
+	}
+	return test;
 }
 
 
